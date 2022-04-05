ed99f387ad61fe8a75cc722fe3c9ba21b0178c8c
Son0-0 <jsmith032211@gmail.com>
2022-04-05 23:38:53 +0900

    2022-04-05

--- etc/15654.py
+++ etc/15654.py
@@ -7,19 +7,22 @@ num_list = [int(num) for num in input().split()]
 result = []
 num_list.sort()
 
-def recur(cur, length, rs):
+def recur(visited, length, rs):
 	if length == 0:
 		global result
 		result.append(rs[:-1])
 		return
 
 	for i in range(n):
-		if num_list[cur] < num_list[i]:
-			recur(i, length - 1, rs + str(num_list[i]) + " ")
-
+		if visited[i] == 0:
+			visited[i] = 1
+			recur(visited, length - 1, rs + str(num_list[i]) + " ")
+			visited[i] = 0
 
 def solution():
-	recur(0, m, "")
+	global result
+	visited = [0] * n
+	recur(visited, m, "")
 	print(*result, sep="\n")
 
 solution()
