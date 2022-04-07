ade55cd6bccaa2d7a99bebb44b12723c38086844
Son0-0 <jsmith032211@gmail.com>
2022-04-07 14:38:05 +0900

    commit

--- week1/38_10971_외판원_순회2.py
+++ week1/38_10971_외판원_순회2.py
@@ -73,3 +73,40 @@ solution()
 #   print(result)
   
 # solution()
+
+# import sys
+# input = sys.stdin.readline
+# size = int(input())
+# llist = [list(map(int, input().split())) for _ in range(size)]
+# min_value = sys.maxsize
+
+# def recur(visited, odr_list, cost):
+#   global min_value
+  
+#   if min_value < cost:
+#     return
+  
+#   if sum(visited) == size:
+#     target = llist[odr_list[-1]][odr_list[0]]
+#     if target != 0:
+#       cost += target
+#       min_value = min(min_value, cost)
+#       return
+  
+#   for i in range(size):
+#     if visited[i] == 0:
+#       target = 0
+#       if 0 < len(odr_list):
+#         target = llist[odr_list[-1]][i]
+#         if target == 0:
+#           continue
+#       visited[i] = 1
+#       recur(visited, odr_list + [i], cost + target)
+#       visited[i] = 0
+      
+# def solution():
+#   visited = [0] * size
+#   recur(visited, [], 0)
+#   print(min_value)
+  
+# solution()
